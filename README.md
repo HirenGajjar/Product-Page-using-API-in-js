dd1659fb12fc450dfbcff90ffb8501542335d8ac
HirenGajjar <gajjarhiren111@gmail.com>
2024-02-23 16:20:32 -0800

    Add script to generate card

--- README.md
+++ README.md
@@ -5,3 +5,8 @@ Here's a simple demo of how to make a dynamic product page using API.
 ## API to use for data
 https://jsonplaceholder.typicode.com/photos
 
+When there is tones of data coming through API, it makes site slow, at that time there are two ways to tackle.
+1. only show data up to screen height
+2. Using back end, add paging
+
+- ?_limit=20 this at the end of the url will limit the data upto 20, it depends on URL, 
--- script.js
+++ script.js
@@ -2,11 +2,25 @@ window.onload = async function() {
     let loader = document.getElementById('loader');
     try {
         loader.style.display = 'block';
-        let res = await fetch("https://jsonplaceholder.typicode.com/photos", {
+        let res = await fetch("https://jsonplaceholder.typicode.com/photos?_limit=20", {
             method: 'GET'
         });
         let data = await res.json();
+        let container = document.getElementById("container");
         console.log(data);
+        for (let i = 0; i < data.length; i++) {
+            let item = data[i];
+            let card = `
+            <div class="card">
+            <img class="image" src="${item.thumbnailUrl}" />
+            
+            
+            <h3>${item.title}</h3>
+            <a class="card-link" href="${item.url}" download="${item.title}.pdf">Get now</a>
+            </div>`;
+
+            container.innerHTML += card;
+        }
 
     } catch {} finally {
         loader.style.display = 'none';
