0c2cb4f890ae282e869d22b514ae7c17b92a3ffe
HirenGajjar <gajjarhiren111@gmail.com>
2024-02-23 17:49:59 -0800

    Add post put method code

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 window.onload = async function() {
+
+
     let loader = document.getElementById('loader');
     try {
         loader.style.display = 'block';
@@ -12,17 +14,69 @@ window.onload = async function() {
             let item = data[i];
             let card = `
             <div class="card">
+
             <img class="image" src="${item.thumbnailUrl}" />
             
-            
+            <div class="card-info">
             <h3>${item.title}</h3>
-            <a class="card-link" href="${item.url}" download="${item.title}.pdf">Get now</a>
+            <a class="card-link" target="_blank" href="${item.url}" download="${item.title}.pdf">Get now</a>
+            <button class="edit-btn" id="${item.id}">Edit</button>
+            </div>
             </div>`;
 
             container.innerHTML += card;
         }
-
-    } catch {} finally {
+        //PUT
+        let editBtn = document.getElementsByClassName("edit-btn");
+        for (let j = 0; j < editBtn.length; j++) {
+            editBtn[j].onclick = async function() {
+                let data = JSON.stringify({
+                    id: 1,
+                    title: 'foo',
+                    body: 'bar',
+                    userId: 1,
+                });
+                let response = await fetch(`https://jsonplaceholder.typicode.com/posts/${this.id}`, {
+                    method: 'PUT',
+                    body: data,
+                    headers: {
+                        'Content-type': 'applicaiton/json; charset=UTF-8'
+                    }
+                })
+                let updatedData = response.json();
+                console.log(updatedData);
+            }
+        }
+    } catch (err) { console.log(err); } finally {
         loader.style.display = 'none';
     }
+    let form = document.getElementById('form');
+    form.addEventListener('submit', async(e) => {
+
+
+        e.preventDefault();
+
+        try {
+            let data = JSON.stringify({
+                title: `${form[0].value}`,
+                body: `${form[1].value}`,
+                userId: `${form[2].value}`
+            });
+
+            console.log(data);
+
+            let response = await fetch("https://jsonplaceholder.typicode.com/posts", {
+                method: 'POST',
+                body: data,
+                headers: {
+                    'Content-type': 'applicaiton/json; charset=UTF-8'
+                }
+            })
+            let responseData = await response.json();
+            console.log(responseData);
+        } catch (err) { console.log(err); } finally {
+
+        }
+
+    })
 }
